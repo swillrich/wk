405bfacdde512894e05ea1444aab75ad8bace52a
Sven <sven.willrich@googlemail.com>
2015-06-22 17:43:07 +0200

    blafasl?

--- src/main/java/de/wk/holiday/Month.java
+++ src/main/java/de/wk/holiday/Month.java
@@ -1,5 +1,7 @@
 package de.wk.holiday;
 
+import org.joda.time.DateTime;
+
 public enum Month {
 	January(31), February(28), March(31), April(30), May(31), June(30), July(31), August(
 			31), September(30), October(31), November(30), December(31);
@@ -7,10 +9,15 @@ public enum Month {
 	private int days;
 
 	Month(int days) {
-		this.days = days;
-		if (name().equalsIgnoreCase("february")) {
-			//check if leap year ...
+		if (name().equalsIgnoreCase("february")
+				&& new DateTime().getYear() % 400 == 0) {
+			days += 1;
 		}
+		this.days = days;
+	}
+
+	public int getDays() {
+		return days;
 	}
 
 }
--- src/main/java/de/wk/holiday/WKYear.java
+++ src/main/java/de/wk/holiday/WKYear.java
@@ -3,10 +3,33 @@ package de.wk.holiday;
 public class WKYear {
 
 	public WKYear() {
-		Month m = Month.February;
+		printYear();
 	}
 
 	public void printYear() {
+		StringBuilder formatBuilder = new StringBuilder();
+		for (int i = 0; i < Month.values().length; i++) {
+			formatBuilder.append("%-10s");
+		}
+		String format = String.format(formatBuilder.toString(), Month.values());
 
+		String underline = "";
+		for (int i = 0; i < format.length(); i++) {
+			underline += "=";
+		}
+
+		System.out.println(format + "\n" + underline);
+
+		for (int i = 1; i <= 31; i++) {
+			String[] days = new String[Month.values().length];
+			for (int currentMonthIndex = 0; currentMonthIndex < days.length; currentMonthIndex++) {
+				if (Month.values()[currentMonthIndex].getDays() >= i) {
+					days[currentMonthIndex] = String.valueOf(i);
+				} else {
+					days[currentMonthIndex] = "-";
+				}
+			}
+			System.out.println(String.format(formatBuilder.toString(), days));
+		}
 	}
 }
