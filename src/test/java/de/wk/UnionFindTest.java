b4f4f4e36b56511656f18fbc4da5ba52c7982276
kretzschi <ksascha89@hotmail.de>
2015-07-08 11:13:47 +0200

    add new tests

--- src/test/java/de/wk/UnionFindTest.java
+++ src/test/java/de/wk/UnionFindTest.java
@@ -16,36 +16,60 @@ public class UnionFindTest {
 	@Before
 	public void setUp() throws Exception {
 		List<Integer> universe = new ArrayList<Integer>();
-		for (int i = 0; i <= 20; i++) {
+		for (int i = 0; i <= 6; i++) {
 			universe.add(i);
 		}
 		this.unionFind = new UnionFind<Integer>(universe);
 	}
 
 	@Test
-	public void testInteger() {
-		System.out.println(this.unionFind.find(1));
-		System.out.println(this.unionFind.find(2));
-		System.out.println(this.unionFind.find(3));
-		System.out.println(this.unionFind.find(4));
-		System.out.println(this.unionFind.find(5));
-		System.out.println("####################");
-		this.unionFind.union(1,2);
-		this.unionFind.union(3,4);
-		this.unionFind.union(1,5);
-		System.out.println(this.unionFind.find(1));
-		System.out.println(this.unionFind.find(2));
-		System.out.println(this.unionFind.find(3));
-		System.out.println(this.unionFind.find(4));
-		System.out.println(this.unionFind.find(5));
-		System.out.println("####################");
-		System.out.println(this.unionFind.getAllValuesByKey(1));
-		System.out.println(this.unionFind.getAllValuesByKey(2));
-		System.out.println(this.unionFind.getAllValuesByKey(3));
-		System.out.println(this.unionFind.getAllValuesByKey(4));
-		System.out.println(this.unionFind.getAllValuesByKey(5));
-		System.out.println(this.unionFind.getAllValuesByKey(6));
+	public void testFindWithSingeElementUnionTrue() {
+		this.unionFind.union(1, 2);
+		assertTrue(this.unionFind.find(1).equals(this.unionFind.find(2)));
 
 	}
 
+	@Test
+	public void testFindWithSingeElementUnionFalse() {
+		this.unionFind.union(1, 2);
+		assertFalse(this.unionFind.find(1).equals(this.unionFind.find(3)));
+
+	}
+
+	@Test
+	public void testFindWithUnionSets() {
+		this.unionFind.union(1, 2);
+		this.unionFind.union(3, 4);
+		this.unionFind.union(1, 3);
+		assertTrue(this.unionFind.find(1).equals(this.unionFind.find(3)));
+		assertTrue(this.unionFind.find(1).equals(this.unionFind.find(4)));
+		assertTrue(this.unionFind.find(2).equals(this.unionFind.find(3)));
+		assertTrue(this.unionFind.find(2).equals(this.unionFind.find(4)));
+
+	}
+
+	@Test
+	public void testInSameSetMethod() {
+		this.unionFind.union(1, 2);
+		assertTrue(this.unionFind.inSameSet(1, 2));
+		assertFalse(this.unionFind.inSameSet(1, 3));
+
+	}
+
+	@Test
+	public void testMethodGetAllValuesByKey() {
+		this.unionFind.union(1, 2);
+		this.unionFind.union(1, 3);
+		this.unionFind.union(2, 4);
+		this.unionFind.union(5, 6);
+		this.unionFind.union(4, 6);
+		ArrayList<Integer> oracle = new ArrayList<Integer>(6);
+		oracle.add(1);
+		oracle.add(2);
+		oracle.add(3);
+		oracle.add(4);
+		oracle.add(5);
+		oracle.add(6);
+		assertTrue(this.unionFind.getAllValuesByElement(1).equals(oracle));
+	}
 }
